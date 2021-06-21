a806ba117fb0ccb2756d9c214377f84ab5b8c696
yasiru-hasaranajala <yasiru583@gmail.com>
2021-06-21 21:35:35 +0530

    menu->serch by image

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import ntpath
 import os
 imgPath = ""
 
-def button1():
+def loadImg(window2_main):
     path=tkFileDialog.askopenfilename(filetypes=[("Image File",'.jpg')])
     im = Image.open(path)
     im.save("test.jpg")
@@ -16,26 +16,46 @@ def button1():
     width, height = im.size
     im = im.resize((int(width*450/height) ,450), Image.ANTIALIAS)
     tkimage = ImageTk.PhotoImage(im)
-    myvar=Label(root,image = tkimage)
+    myvar=Label(window2_main,image = tkimage)
     myvar.image = tkimage
     myvar.grid(row=1,column=0, columnspan=3, sticky="nsew")
 
-def solve(path):
+def solve(path, window2_main):
     solveByImage(path)
     im = Image.open('solved.jpg')
     width, height = im.size
     im = im.resize((int(width*450/height) ,450), Image.ANTIALIAS)
     tkimage = ImageTk.PhotoImage(im)
-    myvar=Label(root,image = tkimage)
+    myvar=Label(window2_main,image = tkimage)
     myvar.image = tkimage
     myvar.grid(row=1,column=0, columnspan=3, sticky="nsew")
 
 
 root = Tk()
-button1 = Button(root,text ='Open Image',command = button1, height=2, width=20)
-button2 = Button(root,text ='Solve',command = lambda: solve("test.jpg"), height=2, width=20)
-button3 = Button(root,text ='Close',command = root.destroy, height=2, width=20)
-button1.grid(row=0, column=0, padx=(20, 20), pady=(20, 20))
-button2.grid(row=0, column=1, padx=(20, 20), pady=(20, 20))
-button3.grid(row=0, column=2, padx=(20, 20), pady=(20, 20))
+root.title("Sudoku Solver")
+def window1(window2_main):
+    window2_main.destroy()
+    window1_main = Tk()
+    window1_main.title("Sudoku Solver")
+    button1 = Button(window1_main,text ='Solve by Image',command = lambda: window2(window1_main), height=2, width=20)
+    button2 = Button(window1_main,text ='Solve by Video',command = lambda: solve("test.jpg"), height=2, width=20)
+    button3 = Button(window1_main,text ='Close',command = window1_main.destroy, height=2, width=20)
+    button1.grid(row=0, column=0, padx=(20, 20), pady=(20, 20))
+    button2.grid(row=1, column=0, padx=(20, 20), pady=(20, 20))
+    button3.grid(row=2, column=0, padx=(20, 20), pady=(20, 20))
+    window1_main.mainloop()
+
+def window2(window1_main):
+    window1_main.destroy()
+    window2_main = Tk()
+    window1_main.title("Solve by Image")
+    button1 = Button(window2_main,text ='Open Image',command = lambda: loadImg(window2_main), height=2, width=20)
+    button2 = Button(window2_main,text ='Solve',command = lambda: solve("test.jpg", window2_main), height=2, width=20)
+    button3 = Button(window2_main,text ='Close',command = lambda: window1(window2_main), height=2, width=20)
+    button1.grid(row=0, column=0, padx=(20, 20), pady=(20, 20))
+    button2.grid(row=0, column=1, padx=(20, 20), pady=(20, 20))
+    button3.grid(row=0, column=2, padx=(20, 20), pady=(20, 20))
+    window2_main.mainloop()
+
+window1(root)
 root.mainloop()
